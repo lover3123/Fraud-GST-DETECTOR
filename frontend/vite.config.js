4cd9b6041a2a168d7f24b02b0ea80e5773b57807
ultron destroyer <userchat12@gmail.com>
2026-04-22 10:17:13 +0530

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Matches your repository name exactly (Case Sensitive)
-  base: '/Fraud-GST-DETECTOR/', 
+  base: '/',
 })
