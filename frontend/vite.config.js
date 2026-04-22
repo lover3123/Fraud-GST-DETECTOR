344f061770b5afb06976c95493c37d74bb070b59
ultron destroyer <userchat12@gmail.com>
2026-04-22 09:56:24 +0530

    env init and packages to run vite front end

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Change '/fraud-detection/' to match your GitHub repository name
+  base: './', 
 })
